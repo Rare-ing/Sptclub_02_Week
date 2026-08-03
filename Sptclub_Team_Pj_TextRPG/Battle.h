5316afdca56c9284c227d2e4baf4c16bf3154e8b
akimelP <tisk1513@gmail.com>
2026-08-03 17:32:35 +0900

    26.08.03 신찬호 오류수정

--- Sptclub_Team_Pj_TextRPG/Battle.h
+++ Sptclub_Team_Pj_TextRPG/Battle.h
@@ -26,8 +26,10 @@ private:
 
 	void GiveReward();
 
+	bool CanUsePotion;
+
 public:
-	Battle(Player*& player, Monster* monster, Inventory* inventory);
+	Battle(Player*& player, Monster* monster, Inventory* inventory, bool CanUsePotion = true);
 	//Player*는 플레이어 객체의 주소를 복사하는 거고,
     //Player*& 는 main에 있는 포인터 자체를 참조하는 것, 즉 *& 사용해야 2차전직 가능해서 수정했음-준우
 
