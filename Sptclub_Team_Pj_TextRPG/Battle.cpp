e140b9a964c0bbee9c82a30fe3d2faf2eb39b027
tmxk9921@gmail.com <54259647+Rare-ing@users.noreply.github.com>
2026-08-03 14:14:07 +0900

    08/03_이희중_몬스터출현메세지 및 HPMP출력자리바꾸기

--- Sptclub_Team_Pj_TextRPG/Battle.cpp
+++ Sptclub_Team_Pj_TextRPG/Battle.cpp
@@ -9,6 +9,7 @@ Battle::Battle(Player* player, Monster* monster, Inventory* inventory)
 
 void Battle::StartBattle()
 {
+	std::cout << monster->getSpawnMessage() << std::endl;
 	std::cout << "[ 전투 시작 ]" << std::endl;
 	bool isWin = false;
 
@@ -91,8 +92,8 @@ void Battle::PlayerTurn()
 		cout << player->getName() << "이(가) " << monster->getName() << "을(를) 공격했다!" << std::endl;
 		cout << monster->getName() << "에게 " << player->getAttack() << "의 피해를 입혔다!" << std::endl;
 		cout << monster->getName() << "의 남은 체력 : " << monster->getHp() << std::endl;
-		cout << "남은 MP : " << player->getMp() << std::endl;
 		cout << "남은 HP : " << player->getHp() << std::endl;
+		cout << "남은 MP : " << player->getMp() << std::endl;
 		monster->TakeDamage(0);//몬스터가 죽었는지 확인
 		break;
 
@@ -101,8 +102,8 @@ void Battle::PlayerTurn()
 		cout << player->getName() << "이(가) " << monster->getName() << "에게 스킬을 사용했다!" << std::endl;
 		cout << monster->getName() << "에게 " << player->getAttack() << "의 피해를 입혔다!" << std::endl;
 		cout << monster->getName() << "의 남은 체력 : " << monster->getHp() << std::endl;
-		cout << "남은 MP : " << player->getMp() << std::endl;
 		cout << "남은 HP : " << player->getHp() << std::endl;
+		cout << "남은 MP : " << player->getMp() << std::endl;
 		monster->TakeDamage(0);//몬스터가 죽었는지 확인
 		break;
 
